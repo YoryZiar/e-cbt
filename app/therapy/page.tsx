55f723b929c4dcace3fadb6428fb87e314fbc89e
yoryziar <111405007+YoryZiar@users.noreply.github.com>
2024-09-08 10:52:31 +0800

    [update] - add threapy page

---
+++ app/therapy/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+function TherapyContent() {
+    return (
+        <div>
+            <h1 className="text-white text-center">Therapy page</h1>
+        </div>
+    )
+}
+
+export default function Therapy() {
+    return (
+        <TherapyContent />
+    )
+}
