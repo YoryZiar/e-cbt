b2f78b7d9f25cf19637a6cc67e888c3e7587b232
yoryziar <111405007+YoryZiar@users.noreply.github.com>
2024-09-20 14:16:48 +0800

    [Fix] - dashboard UI

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -107,7 +107,7 @@ export default async function Dashboard() {
                         </CardContent>
                     </Card> */}
                 </div>
-                <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
+                <div className="grid gap-4 grid-cols-1 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
                     <Card
                         className="xl:col-span-2 bg-secondary border-0 shadow-lg shadow-primary" x-chunk="dashboard-01-chunk-4"
                     >
