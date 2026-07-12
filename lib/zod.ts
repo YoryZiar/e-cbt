450b2af41376521e0bdc9c936cc402a1b2f04b2b
YoryZiar <yoryziar@gmail.com>
2026-07-12 21:39:53 +0800

    fix: resolve sync cookies call and implement strict zod schema validations for credentials

--- lib/zod.ts
+++ lib/zod.ts
@@ -2,10 +2,10 @@ import { object, string } from "zod"
 
 export const signInSchema = object({
     email: string({ message: "Email is required" })
+        .trim()
         .min(1, "Email is required")
         .email("Invalid email"),
     password: string({ message: "Password is required" })
-        // .min(1, "Password is required")
-        // .min(8, "Password must be more than 8 characters")
-        // .max(32, "Password must be less than 32 characters"),
+        .min(8, "Password must be at least 8 characters")
+        .max(32, "Password must be less than 32 characters"),
 })
